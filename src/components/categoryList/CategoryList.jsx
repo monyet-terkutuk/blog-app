ced0ab4e499b15e36be19e7e5a0572774f44e78d
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-09-25 16:04:19 +0700

    add responsive and categories

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import styles from "./categoryList.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
 export const CategoryList = () => {
   return (
-    <div className={styles.container}>Category List</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.backend }` }>
+          <Image src="/style.png" alt='' width={32} height={32} className={styles.image} />
+          style
+        </Link>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.frontend }` }>
+          <Image src="/fashion.png" alt='' width={32} height={32} className={styles.image} />
+          fashion
+        </Link>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.javascript }` }>
+          <Image src="/food.png" alt='' width={32} height={32} className={styles.image} />
+          food
+        </Link>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.go }` }>
+          <Image src="/travel.png" alt='' width={32} height={32} className={styles.image} />
+          travel
+        </Link>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.php }` }>
+          <Image src="/culture.png" alt='' width={32} height={32} className={styles.image} />
+          culture
+        </Link>
+        <Link href="/blog?cat=style" className={ `${styles.category} ${styles.java }` }>
+          <Image src="/coding.png" alt='' width={32} height={32} className={styles.image} />
+          coding
+        </Link>
+      </div>
+    </div>
   )
 }
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export const Featured = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}><b className={styles.bold}>Selamat Datang!</b> Di tempat membaca hal hal yang saya pikirkan :)</h1>
+      <h1 className={styles.title}><b>Selamat Datang!</b> Di tempat membaca hal hal yang saya pikirkan :)</h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
           <Image src="/p1.jpeg" alt='' fill className={styles.image} />
